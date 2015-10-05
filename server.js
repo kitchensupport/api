64ce37a14afcd462ff37090e83df039e8c36381c
Garrick Buckley <gbuckle@purdue.edu>
2015-10-05 14:08:44 -0400

    Added more verbose logging. Includes the IP from which the request comes from, the request made, and the included message if applicable.

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ import indexController from './src/controllers/index';
 const app = express();
 const port = 8000;
 
-app.use(logger('hi'));
+app.use(logger());
 
 app.get('/', indexController);
+
+app.use(logger("Error 404: No route found."))
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,6 +1,7 @@
 export default (message) => {
     return (req, res, next) => {
-        console.log(`Logging ${message}`);
+        console.log(`${req.ip},\t${req.method} ${req.protocol}://${req.hostname}${req.path}${message ? ",\t" + message : ""}`);
+        //TODO: Store request inide a logging table in Postgresql.
         next();
     };
 };
