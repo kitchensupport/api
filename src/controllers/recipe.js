4c2fd695bb46f6835ebf5904cef32cddf1e88892
Garrick <gbuckle@purdue.edu>
2015-10-16 13:00:02 -0400

    Fix the recipe search API call.

--- src/controllers/recipe.js
+++ src/controllers/recipe.js
@@ -3,14 +3,17 @@ import request from 'request';
 import yummly from '../../config/yummly.js';
 
 const router = express();
-const YUMMLY_URL = `http://api.yummly.com/v1/api/recipes?_app_id=${yummly.id}&_app_key=${yummly.key}&maxResult=8`;
+const YUMMLY_BASE_URL = `http://api.yummly.com/v1/api/`;
+const YUMMLY_KEYS = `_app_id=${yummly.id}&_app_key=${yummly.key}`;
+const YUMMLY_PARAMS = `maxResult=8`;
+const YUMMLY_RECIPE_SEARCH_URL = `${YUMMLY_BASE_URL}recipes?${YUMMLY_KEYS}&${YUMMLY_PARAMS}`;
 
 export function routes() {
     return router;
 };
 
 router.get('/recipes/featured', (req, res) => {
-    request({uri: YUMMLY_URL,
+    request({uri: YUMMLY_RECIPE_SEARCH_URL,
         timeout: 15000,
         followRedirect: true,
         maxRedirects: 10,
@@ -20,7 +23,6 @@ router.get('/recipes/featured', (req, res) => {
             res.status(200);
             res.send({status: 'success', data: body});
         } else {
-            console.log(`ID: ${yummly.id}, KEY: ${yummly.key}.`);
             console.log(`ER: ${error}, ${response.statusCode}`);
             res.status(400);
             res.send({
@@ -32,7 +34,7 @@ router.get('/recipes/featured', (req, res) => {
 });
 
 router.get('/recipes/search/:searchTerm', (req, res) => {
-    request({uri: `${YUMMLY_URL}&q=${req.params.searchTerm}`,
+    request({uri: `${YUMMLY_RECIPE_SEARCH_URL}&q=${req.params.searchTerm}`,
         timeout: 15000,
         followRedirect: true,
         maxRedirects: 10,
@@ -53,7 +55,7 @@ router.get('/recipes/search/:searchTerm', (req, res) => {
 });
 
 router.get('/recipes/recipe/:recipeId', (req, res) => {
-    request({uri: `${YUMMLY_URL}&q=${req.params.searchTerm}&recipe-id=${req.params.recipeId}`,
+    request({uri: `${YUMMLY_BASE_URL}recipe/${req.params.recipeId}?${YUMMLY_KEYS}&${YUMMLY_PARAMS}`,
         timeout: 15000,
         followRedirect: true,
         maxRedirects: 10,
